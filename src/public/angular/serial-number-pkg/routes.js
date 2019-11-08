ea261599c24efb460bccf87ca2416662971ed36e
Abdul <abdul@uitouxsolutions.com>
2019-11-08 11:52:53 +0530

    updates

---
+++ src/public/angular/serial-number-pkg/routes.js
@@ -0,0 +1,31 @@
+app.config(['$routeProvider', function($routeProvider) {
+
+    $routeProvider.
+    //SERIAL NUMBER TYPES
+    when('/serial-number-pkg/serial-number-type/list', {
+        template: '<serial-number-type-list></serial-number-type-list>',
+        title: 'Serial Number Types',
+    }).
+    when('/serial-number-pkg/serial-number-type/add', {
+        template: '<serial-number-type-form></serial-number-type-form>',
+        title: 'Add Serial Number Type',
+    }).
+    when('/serial-number-pkg/serial-number-type/edit/:id', {
+        template: '<serial-number-type-form></serial-number-type-form>',
+        title: 'Edit Serial Number Type',
+    })
+
+    //serial-number
+    when('/serial-number-pkg/serial-number-segment/list', {
+        template: '<serial-number-segment-list></serial-number-segment-list>',
+        title: 'Serial Number Segments',
+    }).
+    when('/serial-number-pkg/serial-number-segment/add', {
+        template: '<serial-number-segment-form></serial-number-segment-form>',
+        title: 'Add Serial Number Segment',
+    }).
+    when('/serial-number-pkg/serial-number/edit/:id', {
+        template: '<serial-number-segment-form></serial-number-segment-form>',
+        title: 'Edit Serial Number Segment',
+    });
+}]);
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
-Route::group(['namespace' => 'Abs\CnDnPkg', 'middleware' => ['web', 'auth'], 'prefix' => 'cn-dn-pkg'], function () {
-	Route::get('/dns/get-list', 'DnController@getDnList')->name('getDnList');
-	Route::get('/dn/save', 'DnController@saveDn')->name('saveDn');
+Route::group(['namespace' => 'Abs\SerialNumberPkg', 'middleware' => ['web', 'auth'], 'prefix' => 'serial-number-pkg'], function () {
+	Route::get('/serial-number-segments/get-list', 'SerialNumberSegmentController@getSerialNumberSegmentList')->name('getSerialNumberSegmentList');
+	Route::get('/serial-number-segment/save', 'TaxController@saveSerialNumberSegment')->name('saveSerialNumberSegment');
+
+	Route::get('/serial-number-types/get-list', 'SerialNumberTypeController@getSerialNumberTypeList')->name('getSerialNumberTypeList');
+	Route::get('/serial-number-type/save', 'SerialNumberTypeController@saveSerialNumberType')->name('saveSerialNumberType');
 });
