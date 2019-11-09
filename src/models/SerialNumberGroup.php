bea9c8cb58c5e588ce60c5e88abcc68101afc55e
Abdul <abdul@uitouxsolutions.com>
2019-11-09 15:03:50 +0530

    Create SerialNumberGroup.php

---
+++ src/models/SerialNumberGroup.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Abs\SerialNumberPkg;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SerialNumberGroup extends Model {
+	use SoftDeletes;
+	protected $fillable = [
+		'name',
+		'data_type_id',
+		'created_by_id',
+	];
+}
