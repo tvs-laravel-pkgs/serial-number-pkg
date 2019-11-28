fb17f8f352e30e6ce953fb8f5d053e3ac5b42f8d
Abdul <47030364+kaligan-uitoux@users.noreply.github.com>
2019-11-28 14:18:15 +0530

    Create 2019_11_28_140642_sngsns_u2.php

---
+++ src/migrations/2019_11_28_140642_sngsns_u2.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SngsnsU2 extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		//
+
+		Schema::table('serial_number_group_serial_number_segment', function (Blueprint $table) {
+			$table->dropForeign('sng_snt_u');
+			$table->dropForeign('sng_su');
+			$table->dropUnique('sngsns_unique');
+			$table->foreign('serial_number_group_id', 'sng_snt_u')->references('id')->on('serial_number_types')->onDelete('CASCADE')->onUpdate('cascade');
+			$table->foreign('segment_id', 'sng_su')->references('id')->on('serial_number_segments')->onDelete('CASCADE')->onUpdate('cascade');
+		});
+
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::table('serial_number_group_serial_number_segment', function (Blueprint $table) {
+			$table->unique(["serial_number_group_id", "segment_id"], 'sngsns_unique');
+		});
+		//
+	}
+}
