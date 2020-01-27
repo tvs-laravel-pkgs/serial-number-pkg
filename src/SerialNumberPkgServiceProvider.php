1bd2e3b064442984c7a6046f104ac95037279979
Abdul <abdul@uitoux.in>
2020-01-27 11:45:48 +0530

    u

--- src/SerialNumberPkgServiceProvider.php
+++ src/SerialNumberPkgServiceProvider.php
@@ -17,6 +17,8 @@ class SerialNumberPkgServiceProvider extends ServiceProvider {
 		$this->loadViewsFrom(__DIR__ . '/views', 'serial-number-pkg');
 		$this->publishes([
 			__DIR__ . '/public' => base_path('public'),
+			__DIR__ . '/database/seeds/client' => 'database/seeds',
+			__DIR__ . '/config/config.php' => config_path('serial-number-pkg.php'),
 		]);
 	}
 
